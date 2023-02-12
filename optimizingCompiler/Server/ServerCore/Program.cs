f5f9752f7572f69c0380a3956b205e4246b0f00a
Rachel Kim <hyunjinney@naver.com>
2023-02-12 22:26:54 +0900

    02_12
    Optimizing Compiler
    Release 모드로 변환시 코드최적화 때문에 일어나지 않던 버그들 발생 가능

---
+++ optimizingCompiler/Server/ServerCore/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+        //최적화하지말라는 뜻
+        volatile static bool _stop = false; 
+
+        static void ThreadMain()
+        {
+            Console.WriteLine("쓰레드 시작!"); 
+            while(_stop==false)
+            {
+                //누군가가 stop 신호 해주기를 기다린다
+            }
+
+            Console.WriteLine("쓰레드 종료!"); 
+        }
+        static void Main(string[] args)
+        {
+            Task t = new Task(ThreadMain);
+            t.Start();
+
+            Thread.Sleep(1000);
+
+            _stop = true;
+
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기 중");
+
+            t.Wait();
+
+            Console.WriteLine("종료 성공");
+        }
+    }
+}
