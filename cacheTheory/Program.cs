37f6c36124365bb8e63bbe401b30eec8868311ba
Rachel Kim <hyunjinney@naver.com>
2023-02-14 10:28:53 +0900

    02_14
     cache작동하는 거 확인해보기
    Temporal locality/ Spatial locality

---
+++ cacheTheory/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[10000, 10000];
+
+            {
+                //cash hit한 상황
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[y, x] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y,x) 순서 걸린 시간 {end-now}");
+            }
+
+            {
+                //공간적 이점을 살릴 수 없는 경우, 시간이 첫번째보다 거의 배가 걸림
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[x, y] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x,y) 순서 걸린 시간 {end - now}");
+            }
+
+
+        }
+    }
+}
