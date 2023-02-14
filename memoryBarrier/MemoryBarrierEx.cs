86e275fef314cae23858c3e56d717f9b68179bdd
Rachel Kim <hyunjinney@naver.com>
2023-02-14 11:58:03 +0900

    02_14
    memory barrier
    하드웨어도 최적화를 해주고 있음
    multi thread 상황에서 주의 필요
    가시성 보장을 위해 Thread.MemoryBarrier();

---
+++ memoryBarrier/MemoryBarrierEx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class MemoryBarrierEx
+    {
+        int _answer;
+        bool _complete;
+
+        void A()
+        {
+            //연속적으로 store -> 둘다 memorybarrier를 해줘야 확실하게 해줄 수 있음
+            _answer = 123;
+            Thread.MemoryBarrier(); // Barrier 1
+            _complete = true;
+            Thread.MemoryBarrier();// Barrier 2
+        }
+
+        void B()
+        {
+            Thread.MemoryBarrier();// Barrier 3
+            if (_complete)
+            {
+                Thread.MemoryBarrier();// Barrier 4
+                Console.WriteLine(_answer);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
