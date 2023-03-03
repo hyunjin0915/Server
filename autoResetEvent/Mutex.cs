92231e4cee077003952aeb650612c5deb9dd7b74
Rachel Kim <hyunjinney@naver.com>
2023-03-03 22:44:41 +0900

    Mutex

---
+++ autoResetEvent/Mutex.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static int _num = 0;
+
+        //다른점 : 락 횟수 카운트->그만큼 release되게 / 락-릴리즈 짝이 맞지 않은지 검사... 비용이 커짐
+        //그냥 있다고만 알고 있으면 된다. 커널단위 작동, 느리다
+        static Mutex _lock = new Mutex();
+
+        static void Thread_1()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                _lock.WaitOne();
+                _num++;
+                _lock.ReleaseMutex();
+            }
+        }
+
+        static void Thread_2()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                _lock.WaitOne();
+                _num--;
+                _lock.ReleaseMutex();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(_num);
+        }
+    }
+}
