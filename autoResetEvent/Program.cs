8263961cc1136879cc618deee90943665ef813a6
Rachel Kim <hyunjinney@naver.com>
2023-03-03 22:32:39 +0900

    AutoResetEvent
    23_03_03
    대기 중인 쓰레드에게 외부에서 signal을 보내 쓰레드 흐름 통제하는 방식
    WaitOne() - 대기
    Set() - flag=true/ 대기 해제

---
+++ autoResetEvent/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Lock
+    {
+        //커널에서 관리하는 bool이라 생각, 문닫기 자동으로 해줌
+        AutoResetEvent _available = new AutoResetEvent(true);
+
+        public void Acquire()
+        {
+            _available.WaitOne();//입장시도
+        }
+
+        public void Release()
+        {
+            _available.Set(); //flag = true
+        }
+    }
+    class Program
+    {
+        static int _num = 0;
+        static Lock _lock = new Lock();
+
+        static void Thread_1()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                _lock.Acquire();
+                _num++;
+                _lock.Release();
+            }
+        }
+
+        static void Thread_2()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                _lock.Acquire();
+                _num--;
+                _lock.Release();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(_num);
+        }
+    }
+}
